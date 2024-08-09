877a32f08da7e2a4966027148964e4e110f64d6d
acho98 <sdfgh98@naver.com>
2024-08-09 09:56:25 +0900

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -15,7 +15,7 @@ load_dotenv()
 openai_api_key = os.getenv('OPENAI_API_KEY')
 
 st.set_page_config(page_title="simanpdf", page_icon="")
-st.title("심안(心眼)봇")
+st.title("라이나봇")
 
 def get_session_history(session_id: str) -> BaseChatMessageHistory:
     if session_id not in st.session_state["store"]:
